bddc9bd11a70efccdf222c304cdf73256a3dc5cf
digital-synapse <digital.synapse.software@gmail.com>
2014-12-03 00:50:33 -0700

    :)

---
+++ angular-dropdown/angular-dropdown.js
@@ -0,0 +1,75 @@
+﻿angular.module('angular-dropdown', [])
+
+.directive('dropdown', function ($compile) {
+    return {
+        restrict: 'E',
+        link: function (scope, element, attrs) {
+
+            // assumes scope.item is defined
+            var listname = scope.dropdown.binding[attrs.model];
+            if (!listname) throw 'unable to bind to list!';
+            var labelname = scope.dropdown.label[listname];
+
+            element.html(
+                '<select ng-model="dropdown.model.' + attrs.model +
+                '" ng-options="' + attrs.model +
+                '.' + labelname + ' for ' + attrs.model +
+                ' in dropdown.list.' + listname +
+                '"/>').show();
+            $compile(element.contents())(scope);
+        }
+    };
+})
+
+.factory('dropdown', function ($rootScope) {
+    var service = {
+        model: {},
+        key: {},
+        label: {},
+        list: {
+            add: function (name, array) {
+                service.list[name] = array;
+                return service;
+            }
+        },
+        binding: {
+            add: function (parent, prop, name, listname, keyname, labelname) {
+                if (!parent) parent = {};
+                listname = listname || name + 's';
+                service.key[name] = keyname || 'id';
+                service.label[listname] = labelname || 'name';
+                service.model[name] = {};
+                service.binding[name] = listname;
+                service.binding[listname] = name;
+                var key = service.key[name] || service.key[service.binding[name]];
+                $rootScope.$watch(function () {
+                    return service.model[name];
+                }, function watchCallback(newValue, oldValue) {
+                    parent[prop] = newValue[key];
+                });
+                service.select(name, parent[prop]);
+                return service;
+            }
+        },
+        find: function (name, id) {
+            var array = service.list[name] || service.list[service.binding[name]];
+            if (!array) throw 'dropdown service could not find a list for: ' + name;
+            var key = service.key[name] || service.key[service.binding[name]];
+            var i = array.length;
+            while (i--) {
+                if (array[i][key] == id) {
+                    return array[i];
+                }
+            }
+        },
+        select: function (name, id) {
+            service.model[name] = service.find(name, id);
+            return service;
+        },
+        selected: function (name) {
+            var key = service.key[name] || service.key[service.binding[name]];
+            return service.model[name][key];
+        }
+    };
+    return service;
+});
---
+++ angular-dropdown/app.js
@@ -0,0 +1,46 @@
+﻿angular.module('app', ['angular-dropdown'])
+
+.factory('MockFormResourceFactory', function () {
+    return {
+        get: function (callback) {
+            callback({
+                AnimalTypeId1: 1,
+                AnimalTypeId2: 3
+            });
+        },
+        save: function (item) {
+            alert(JSON.stringify(item, null, '    '));
+        }
+    };
+})
+.factory('MockListResourceFactory', function () {
+    return {
+        get: function (callback) {
+            callback([
+                { id: 1, name: 'Red Fox' },
+                { id: 2, name: 'Yellow Bird' },
+                { id: 3, name: 'Brown Dog' }
+            ]);
+        }
+    };
+})
+
+.controller('ctrl', function ($scope, dropdown, MockFormResourceFactory, MockListResourceFactory) {
+    $scope.dropdown = dropdown;
+
+    $scope.form = {};
+
+    MockListResourceFactory.get(function (list) {
+        dropdown.list.add('options', list);
+
+        MockFormResourceFactory.get(function (form) {
+            $scope.form = form;
+            dropdown.binding.add($scope.form,'AnimalTypeId1', 'option1', 'options');
+            dropdown.binding.add($scope.form, 'AnimalTypeId2', 'option2', 'options');
+        });
+    });
+
+    $scope.submit = function () {
+        MockFormResourceFactory.save($scope.form);
+    };
+});
